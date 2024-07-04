7eddfed0f0a98742f5002008a9657d6b577b8729
shubham <shubhamsingh1295@yahoo.in>
2024-07-04 21:12:05 +0530

    feat: initial commit

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
+      def index
+        @users = User.all
+        render json: @users
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def filter
+        campaign_names = params[:campaign_names].split(',')
+        conditions = campaign_names.map do |name|
+          "JSON_CONTAINS(campaigns_list, '{\"campaign_name\":\"#{name}\"}', '$')"
+        end.join(' OR ')
+
+        @users = User.where(conditions)
+        render json: @users
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email, campaigns_list: [:campaign_name, :campaign_id])
+      end
+    end
+  end
+end
